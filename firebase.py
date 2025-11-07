f0147775ac447496a26b6d7eb8a807cdd7b5e200
JohnLloydProg <johnlloydunida0@gmail.com>
2025-11-07 17:29:58 +0800

    implemented locks on the cloud processes

--- firebase.py
+++ firebase.py
@@ -8,6 +8,8 @@ from time import sleep
 import json
 import os
 
+LOCKED_OPERATION = {"write": ["write", "read"], "read":["write"]}
+
 firebaseConfig = {
   "apiKey": "AIzaSyC_DMlTx9Ipc9yx9IPA3lVeITXZD0QhwpE",
   "authDomain": "cloudos-12cdc.firebaseapp.com",
@@ -42,6 +44,25 @@ class Firebase:
     def __init__(self, computer:Computer):
         self.computer = computer
 
+    def lock_path(self, user:User, cloud_path:str, operation:str):
+        if (operation not in ['write', 'read']):
+            raise ValueError(f"{operation} is among the list of operations allowed to lock")
+
+        lock_ref = cloud_path.replace("/", "&456").replace(".", "&123")
+        locks:dict[str, dict[str, str]] = self.db.child("locks").child(user.localId).get(token=user.idToken).val()
+        if (not locks):
+            locks = {}
+        while (operation in LOCKED_OPERATION.get(locks.get(lock_ref), [])):
+            locks = self.db.child("locks").child(user.localId).get(token=user.idToken).val()
+            print(f"Can't process because the path {cloud_path} is locked by a different operation")
+            sleep(1)
+        locks[lock_ref] = operation
+        self.db.child("locks").child(user.localId).update(locks, token=user.idToken)
+        return lock_ref
+
+    def unlock_path(self, user:User, lock_ref:str):
+        self.db.child("locks").child(user.localId).child(lock_ref).set(None, token=user.idToken)
+
     def login(self, email:str, password:str) -> User:
         result = self.auth.sign_in_with_email_and_password(email, password)
         user = User(email, password)
@@ -56,6 +77,11 @@ class Firebase:
         cloud_path = cloud_path.strip("/")
         path = cloud_path.split("/")
         file_name = path.pop()
+
+        lock_ref = self.lock_path(user, cloud_path, 'write')
+        if (not lock_ref):
+            return
+
         upload_process = UploadProcess(firebaseConfig["storageBucket"], user, cloud_path, file_path)
         self.computer.add_process(upload_process)
         while (not upload_process.is_completed()):
@@ -64,6 +90,7 @@ class Firebase:
 
         data = {file_name.replace(".", "&123"):{'type':'file', 'modified':datetime.now().isoformat()}}
         self.db.child('users').child(user.localId).child('owned_files').child(*tuple(path)).update(data, token=user.idToken)
+        self.unlock_path(user, lock_ref)
 
     def get_owned_files(self, user:User) -> dict:
         files = self.db.child('users').child(user.localId).child('owned_files').get(token=user.idToken).val()
@@ -85,12 +112,20 @@ class Firebase:
 
     def update_file(self, user:User, cloud_path:str, file_path:str):
         cloud_path = cloud_path.strip("/")
+
+        #locks the cloud path before processing
+        lock_ref = self.lock_path(user, cloud_path, 'write')
+        if (not lock_ref):
+            return
+        
         process = UploadProcess(firebaseConfig["storageBucket"], user, cloud_path, file_path)
         self.computer.add_process(process)
         while (not process.is_completed()):
             print("Uploading...")
             sleep(0.5)
         self.db.child('users').child(user.localId).child('owned_files').child(*tuple(cloud_path.replace(".", "&123").split("/"))).update({'modified':datetime.now().isoformat()}, token=user.idToken)
+        
+        self.unlock_path(user, lock_ref)
 
     def delete_owned_file(self, user:User, cloud_path:str):
         cloud_path = cloud_path.strip("/")
@@ -117,10 +152,14 @@ class Firebase:
         if (cache_meta.get("modified") != file.get('modified', '')):
             print('file is outdated')
             url = self.storage.child(f'files/{user.localId}/{cloud_path}').get_url(user.idToken)
+            lock_ref = self.lock_path(user, cloud_path, 'read')
+            if (not lock_ref):
+                return
             process = DownloadProcess(url, user, cloud_path)
             self.computer.add_process(process)
             while (not process.is_completed()):
                 sleep(0.5)
+            self.unlock_path(user, lock_ref)
             try:
                 with open(f"{os.environ.get("CACHE_PATH")}/meta/{cloud_path.replace(".", "&123")}.json", 'w') as f:
                     f.write(json.dumps(file))
@@ -137,6 +176,6 @@ class Firebase:
         CustomThread(self.get_file, args=(user, cloud_path), on_finish=on_finish).start()
     
     def update_thread(self, user:User, cloud_path:str, file_path:str, on_finish:Callable):
-        CustomThread(self.update_file, args=(user, cloud_path, file_path), on_finish=on_finish)
+        CustomThread(self.update_file, args=(user, cloud_path, file_path), on_finish=on_finish).start()
 
 
--- test.py
+++ test.py
@@ -17,8 +17,6 @@ user = firebase.login("johnlloydunida0@gmail.com", "password")
 
 firebase.get_thread(user, 'hotdog.txt', lambda result: print(result))
 sleep(3)
-firebase.get_thread(user, 'documents/hello.txt', lambda result: print(result))
-sleep(5)
-firebase.get_thread(user, 'documents/minecraft.txt', lambda result: print(result))
+firebase.update_thread(user, 'hotdog.txt', './test.txt', lambda result: print(result))
 
 input("waiting... enter to end")
